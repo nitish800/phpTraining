7ef4fd97dd842809a1dc63eb257ab9def1c5ff2c
Nitish shah <nitishshah700@gmail.com>
2016-09-08 15:16:44 +0530

    learn about date function

---
+++ day2/date.php
@@ -0,0 +1,29 @@
+<html>
+<head><title>PHP Example</title></head>
+<body>
+<h1>Php Example 13</h1>
+
+<div class="pas"><b>date() in php:</b></div>
+<br>
+<br>
+<?php
+	
+
+echo "<br><div class='pa'> Today is"." ".date('l jS \of F Y')."</div>";
+?>
+<br>
+
+<style>
+h1{
+	text-align:center;
+}
+.pas{
+	text-align:center;
+}
+.pa{
+	text-align:center;
+}
+</style>
+</body>
+
+</html>
