0450df6943c7bf5d11e43c1074a5ab1c87c6edf4
Nitish shah <nitishshah700@gmail.com>
2016-09-10 11:59:29 +0530

    display user data based on roll

---
+++ day4/Database_display.php
@@ -0,0 +1,117 @@
+<html>
+<head><title>PHP Example</title></head>
+<body bgcolor="#eceff1">
+<h1>Php Database Example</h1>
+
+<div class="pas"><b>User Info:</b></div>
+<br>
+<br>
+<div class="pa">
+<?php
+//Retriving value from user or form
+// retrive using $_POST[] $_GET[] $_REQUEST[] 
+
+$roll = $_REQUEST["roll"];  									//taking input name from the FORM
+#$address = $_REQUEST["addre"];
+#$gender = $_REQUEST["gender"];
+#$course = $_REQUEST["course"];
+#$fees = $_REQUEST["fees"];
+
+									//database Connectivity
+
+$host = "host=127.0.0.1"; 											//postgress server ip
+$port = "port=5432"; 												//port address
+$dbname = "dbname=Php_Project"; 									//database name
+$credential = "user=postgres password=###alpha@@@123"; 				//username and password
+
+
+									//creating a database connectivity sesion and storing it in variable $db
+
+
+$db = pg_connect("$host $port $dbname $credential");
+
+									//Checking for conection error.
+
+	if(!$db){
+	echo "<br>Connection Error!!";
+	}
+		
+	//creating a sql statement string and store it in a variable $sql .
+	
+$sql ="Select * from Student where roll=$roll";
+
+ //executing the $sql
+ 
+ $result = pg_query($db,$sql);  //pg_query(<conection>,<query>); return effected no row in update,insert,delete. 
+								// when use "select" query, returns a array of row and coloms.
+			
+
+
+?>
+<table width="500px"  height="300px" border="1px" bgcolor="#ede7f6"  >
+
+
+<tr bgcolor="#b39ddb">
+	<th colspan="6">Student Details.</th>
+	
+</tr>
+<tr>
+	<td class="row1" >Roll</td>
+	<td class="row1" >Name</td>
+	<td class="row1" >Address</td>
+	<td class="row1" >Gender</td>
+	<td class="ros1" >course</td>
+	<td class="row1" >Feee</td>
+</tr>
+<?php
+if($result){
+while($result_data = pg_fetch_row($result))
+{
+echo "<tr>";
+echo "<td class='row1' >".$result_data[0]."</td>";
+echo "<td class='row1' >".$result_data[1]."</td>";
+echo "<td class='row1' >".$result_data[2]."</td>";
+echo "<td class='row1' >".$result_data[3]."</td>";
+echo "<td class='row1' >".$result_data[4]."</td>";
+echo "<td class='row1' >".$result_data[5]."</td>";
+echo "</tr>";
+}
+}else{
+	echo "Error Occour!!";
+}
+	
+							//pg_close terminate the session
+pg_close($db);							
+?>
+</table>
+</div>
+
+<br>
+
+<style>
+h1{
+	text-align:center;
+	color:#000000;
+}
+.pas{
+	text-align:center;
+	font-size:50px;
+	color:#000000;
+	
+}
+.pa{
+	margin-left:400px;
+}
+.inp_size{
+	width:200px;
+}
+.row1{
+	text-align:center;
+}
+</style>
+</body>
+
+</html>
+
+
+
