62867a21cbb8de5f903b75c4e3119bf2dcacb086
Nitish shah <nitishshah700@gmail.com>
2016-09-14 21:07:54 +0530

    display student record and allow to edit it

---
+++ day5/student/display_html.php
@@ -0,0 +1,59 @@
+<html>
+<head><title>PHP Example</title></head>
+<body bgcolor="#e53935">
+<h1>Php Database Example</h1>
+
+<div class="pas"><b>Search User Info Based On Roll No:</b></div>
+<br>
+<br>
+<div class="pa">
+<form id="form1" name="form1" method="post" action="Database_display.php">
+
+<table width="400px"  height="300px"  bgcolor="#ffcdd2"  >
+
+
+<tr bgcolor=#f44336>
+	<th colspan="2">Search User Info</th>
+	
+</tr>
+<tr>
+	<td class="row1" >Enter Roll</td>
+	<td class="row1" ><input type="text" name="roll" class="inp_size" /></td>
+</tr>
+<tr>
+	<td colspan="2" align="center"><input type="Submit" name="Submit" value="Search"/><input type="Reset" value="Reset"/></td>
+</tr>
+
+</table>
+</form>
+</div>
+
+<?php
+	
+?>
+<br>
+
+<style>
+h1{
+	text-align:center;
+	color:#ffffff;
+}
+.pas{
+	text-align:center;
+	font-size:50px;
+	color:#ffffff;
+	
+}
+.pa{
+	margin-left:500px;
+}
+.inp_size{
+	width:200px;
+}
+.row1{
+	text-align:center;
+}
+</style>
+</body>
+
+</html>
