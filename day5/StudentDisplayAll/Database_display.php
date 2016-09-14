7474cd1ce9184c19f02e18b75a6383bf562a351b
Nitish shah <nitishshah700@gmail.com>
2016-09-14 21:08:27 +0530

    display detail of all student and allows to edit it

---
+++ day5/StudentDisplayAll/Database_display.php
@@ -0,0 +1,121 @@
+<html>
+<head><title>PHP Example</title></head>
+<body bgcolor="#eceff1">
+<h1>Php Database Example</h1>
+
+<div class="pas"><b>User Info:</b></div>
+<br>
+<br>
+<div class="pa">
+<?php
+#include('display_html.php');                       //includes the other page in one page.
+//Retriving value from user or form
+// retrive using $_POST[] $_GET[] $_REQUEST[] 
+
+ 									//taking input name from the FORM
+#$address = $_REQUEST["addre"];
+#$gender = $_REQUEST["gender"];
+#$course = $_REQUEST["course"];
+#$fees = $_REQUEST["fees"];
+
+									//database Connectivity
+
+$host = "host=127.0.0.1"; 											//postgress server ip
+$port = "port=5432"; 												//port address
+$dbname = "dbname=Php_Project"; 									//database name
+$credential = "user=postgres password=###alpha@@@123"; 				//username and password
+
+
+									//creating a database connectivity sesion and storing it in variable $db
+
+
+$db = pg_connect("$host $port $dbname $credential");
+
+									//Checking for conection error.
+
+	if(!$db){
+	echo "<br>Connection Error!!";
+	}
+		
+	//creating a sql statement string and store it in a variable $sql .
+	
+$sql ="Select * from Student";
+
+ //executing the $sql
+ 
+ $result = pg_query($db,$sql);  //pg_query(<conection>,<query>); return effected no row in update,insert,delete. 
+								// when use "select" query, returns a array of row and coloms.
+			
+
+
+?>
+<table width="500px"  height="300px" border="1px" bgcolor="#ede7f6"  >
+
+
+<tr bgcolor="#b39ddb">
+	<th colspan="7">Student Details.</th>
+	
+</tr>
+<tr>
+	<td class="row1" >Roll</td>
+	<td class="row1" >Name</td>
+	<td class="row1" >Address</td>
+	<td class="row1" >Gender</td>
+	<td class="ros1" >Course</td>
+	<td class="row1" >Fees</td>
+	<td class="row1" >Edit</td>
+
+</tr>
+<?php
+if($result){
+while($result_data = pg_fetch_row($result))
+{
+echo "<tr>";
+echo "<td class='row1' >".$result_data[0]."</td>";
+echo "<td class='row1' >".$result_data[1]."</td>";
+echo "<td class='row1' >".$result_data[2]."</td>";
+echo "<td class='row1' >".$result_data[3]."</td>";
+echo "<td class='row1' >".$result_data[4]."</td>";
+echo "<td class='row1' >".$result_data[5]."</td>";
+echo "<td class='row1' ><a href='update.php?rollid=".$result_data[0]."'><img src='edit.png' width='20px' height='20px'></a></td>";
+echo "</tr>";
+}
+}else{
+	echo "Error Occour!!";
+}
+	
+							//pg_close terminate the session
+pg_close($db);							
+?>
+</table>
+</div>
+
+<br>
+
+<style>
+h1{
+	text-align:center;
+	color:#000000;
+}
+.pas{
+	text-align:center;
+	font-size:50px;
+	color:#000000;
+	
+}
+.pa{
+	margin-left:400px;
+}
+.inp_size{
+	width:200px;
+}
+.row1{
+	text-align:center;
+}
+</style>
+</body>
+
+</html>
+
+
+
---
+++ day5/StudentDisplayAll/update.php
@@ -0,0 +1,77 @@
+<html>
+<head><title>PHP Example</title></head>
+<body bgcolor="#e53935">
+<h1>Php Database Example</h1>
+
+<div class="pas"><b>Info Update Form</b></div>
+<br>
+<br>
+<div class="pa">
+<form id="form1" name="form1" method="post" action="update_address.php">
+
+<table width="400px"  height="300px"  bgcolor="#ffcdd2"  >
+
+
+<tr bgcolor=#f44336>
+	<th colspan="2">Update Form</th>
+	
+</tr>
+<tr>
+	<td class="row1" >Enter Roll</td>
+	
+	<td class="row1" ><input type="text" name="roll" value="<?php echo $_REQUEST["rollid"]; ?>" readonly="readonly" disabled="disabled"  class="inp_size" /></td>
+</tr>
+<tr>
+	<td class="row1" >Address:</td>
+	<td class="row1"><textarea name="addre" id="text_area" class="inp_size" ></textarea></td>
+</tr>
+<tr>
+	<td class="row1" >Enter Name</td>
+	<td class="row1" ><input type="text" name="name" class="inp_size" /></td>
+</tr>
+<tr>
+	<td class="row1" >Enter Fees</td>
+	<td class="row1" ><input type="text" name="fees" class="inp_size" /></td>
+
+</tr>
+<tr>
+	<td class="row1" >Enter Course</td>
+	<td class="row1" ><input type="text" name="course" class="inp_size" /></td>
+</tr>
+<tr>
+	<td colspan="2" align="center"><input type="Submit" name="Submit" value="UPDATE"/><input type="Reset" value="Reset"/></td>
+</tr>
+
+</table>
+</form>
+</div>
+
+<?php
+	
+?>
+<br>
+
+<style>
+h1{
+	text-align:center;
+	color:#ffffff;
+}
+.pas{
+	text-align:center;
+	font-size:50px;
+	color:#ffffff;
+	
+}
+.pa{
+	margin-left:500px;
+}
+.inp_size{
+	width:200px;
+}
+.row1{
+	text-align:center;
+}
+</style>
+</body>
+
+</html>
